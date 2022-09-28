aa818214d3c5b6c200bab68fd170ab7eb87e2de5
owari <jgabrielsabatini@outlook.com>
2022-09-28 12:10:19 -0300

    Mappings refactored

--- Mappings.cpp
+++ Mappings.cpp
@@ -3,10 +3,9 @@
 #include "Settings.hpp"
 
 Mappings::Mappings() {
-    // init lcd
-    Mappings::lcd = new LCD(LCD_0_RS_PIN, LCD_0_ENABLE_PIN, LCD_0_D4_PIN, LCD_0_D5_PIN, LCD_0_D6_PIN, LCD_0_D7_PIN);
+    Mappings::lcd = new LCD(LCD_0_RS_PIN, LCD_0_ENABLE_PIN, LCD_0_D4_PIN, LCD_0_D5_PIN,
+                            LCD_0_D6_PIN, LCD_0_D7_PIN);
 
-    // init buttons
     uint8_t buttonPins[] = { BANK_0_BUTTON_PIN, BANK_1_BUTTON_PIN, BANK_2_BUTTON_PIN, 
                              BANK_3_BUTTON_PIN, EFFECT_0_BUTTON_PIN, EFFECT_1_BUTTON_PIN,
                              EFFECT_2_BUTTON_PIN, EFFECT_3_BUTTON_PIN, OFFSET_0_BUTTON_PIN,
@@ -17,7 +16,6 @@ Mappings::Mappings() {
         Mappings::buttons[i] = new Button(buttonPins[i]);
     }
 
-    // init leds
     uint8_t ledPins[] = { BANK_0_LED_PIN, BANK_1_LED_PIN, BANK_2_LED_PIN, BANK_3_LED_PIN,
                           EFFECT_0_LED_PIN, EFFECT_1_LED_PIN, EFFECT_2_LED_PIN, EFFECT_3_LED_PIN,
                           POWER_LED_PIN, POTENTIOMETER_LED_PIN };
@@ -27,10 +25,6 @@ Mappings::Mappings() {
         Mappings::leds[i] = new LED(ledPins[i]);
     }
 
-    // init midi trough serial
-    Mappings::midiSerial = new MidiS();
-
-    // init potentiometer
     Mappings::potentiometer = new Potentiometer(POTENTIOMETER_PIN);
 }
 
@@ -38,7 +32,6 @@ Mappings::~Mappings() {
     delete[] Mappings::buttons;
     delete Mappings::lcd;
     delete[] Mappings::leds;
-    delete Mappings::midiSerial;
     delete Mappings::potentiometer;
 }
 
@@ -59,10 +52,6 @@ LCD* Mappings::getLCD() {
 
 LED* Mappings::getLED(uint8_t index) {
     return Mappings::leds[index];
-}
-
-MidiS* Mappings::getMidiSerial() {
-    return Mappings::midiSerial;
 }
 
 Potentiometer* Mappings::getPotentiometer() {
--- Mappings.hpp
+++ Mappings.hpp
@@ -5,44 +5,16 @@
 #include "Button.hpp"
 #include "LCD.hpp"
 #include "LED.hpp"
-#include "MidiS.hpp"
 #include "Potentiometer.hpp"
-
-#define BANK_0_BUTTON 0
-#define BANK_1_BUTTON 1
-#define BANK_2_BUTTON 2
-#define BANK_3_BUTTON 3
-#define EFFECT_0_BUTTON 4
-#define EFFECT_1_BUTTON 5
-#define EFFECT_2_BUTTON 6
-#define EFFECT_3_BUTTON 7
-#define OFFSET_0_BUTTON 8
-#define OFFSET_1_BUTTON 9 
-
-#define BANK_0_LED 0
-#define BANK_1_LED 1
-#define BANK_2_LED 2
-#define BANK_3_LED 3
-#define EFFECT_0_LED 4
-#define EFFECT_1_LED 5
-#define EFFECT_2_LED 6
-#define EFFECT_3_LED 7
-#define POWER_LED 8
-#define POTENTIOMETER_LED 9 
 
 class Mappings { // Harware Mappings and Instantiation
 private:
-    uint8_t buttonNumber;
     Button** buttons;
-
     LCD* lcd;
-
-    uint8_t ledNumber;
     LED** leds;
-
-    MidiS* midiSerial;
-
     Potentiometer* potentiometer;
+    uint8_t ledNumber;
+    uint8_t buttonNumber;
 
 public:
     Mappings();
@@ -52,7 +24,6 @@ public:
     Button* getButton(uint8_t index);
     LCD* getLCD();
     LED* getLED(uint8_t index);
-    MidiS* getMidiSerial();
     Potentiometer* getPotentiometer();
 };
 
